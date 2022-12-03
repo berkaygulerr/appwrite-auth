405a58b554e72ef67065bdf808e3042bb4a373dc
berkayguler <berkay4058@gmail.com>
2022-12-04 01:03:07 +0200

    change: project id

--- src/services/appwriteConfig.js
+++ src/services/appwriteConfig.js
@@ -2,7 +2,7 @@ import { Client, Account, ID } from "appwrite";
 
 const client = new Client()
   .setEndpoint("http://localhost/v1")
-  .setProject("638658e879bd691b07dc");
+  .setProject("projectID");
 
 export const id = ID.unique();
 export const account = new Account(client);
